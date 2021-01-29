7495b5acbb3b91c727f8d4bd84e03ad55dc45385
zhaobo <xtype@qq.com>
2021-01-29 13:42:32 +0800

    对Message增加可解码方法

--- message.go
+++ message.go
@@ -1,7 +1,16 @@
 package webreal
 
+import "encoding/json"
+
 // 客户端发来的消息格式
 type Message struct {
-	Type string      `json:"type"`
-	Data interface{} `json:"data"`
+	Type string           `json:"type"`
+	Data *json.RawMessage `json:"data"`
+}
+
+func (m *Message) ReadMessageData(x interface{}) error {
+	if x == nil {
+		return nil
+	}
+	return json.Unmarshal(*m.Data, x)
 }
